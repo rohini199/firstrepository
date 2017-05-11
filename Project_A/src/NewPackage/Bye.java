6afb19cc87604ef1c78ef65291330330972099a2
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-11 15:32:18 +0530

    Initial project creation

---
+++ Project_A/src/NewPackage/Bye.java
@@ -0,0 +1,10 @@
+package NewPackage;
+
+public class Bye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project_A/src/NewPackage/thirdclass.java
@@ -0,0 +1,10 @@
+package NewPackage;
+
+public class thirdclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
